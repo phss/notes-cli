0878e68a5b9c7a9866e8336097b3048cc7d960e7
Paulo Schneider <paulo.schneider@gmail.com>
2016-04-09 20:21:12 +0100

    Read config file

--- notes-cli.py
+++ notes-cli.py
@@ -1,1 +1,23 @@
-print "Nothing to see here"
+import argparse
+import yaml
+from os.path import expanduser
+
+def load_config_from(path):
+  with open(expanduser(path)) as file:
+    return yaml.load(file)
+
+
+def parse_options():
+  parser = argparse.ArgumentParser()
+  parser.add_argument("command", choices=["ls", "add", "rm", "edit", "view", "reindex"])
+  parser.add_argument("--query")
+  return parser.parse_args()
+
+def main():
+  config = load_config_from("~/.notes-cli/config.yaml")
+  print config
+  options = parse_options()
+  print options
+
+if __name__ == "__main__":
+  main()
