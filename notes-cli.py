b5d2ab56022e05685b30ff108ca8d207ee238a26
Paulo Schneider <paulo.schneider@gmail.com>
2016-04-09 23:30:12 +0100

    Reindex notes

--- notes-cli.py
+++ notes-cli.py
@@ -1,10 +1,23 @@
 import argparse
 import yaml
+import shutil
 import os
 from os.path import expanduser, isdir
 import whoosh.index as ix
 from whoosh.fields import *
 
+
+def command_reindex(index_full_path, notes_path):
+  shutil.rmtree(index_full_path, True)
+  os.mkdir(index_full_path)
+  schema = Schema(filename=TEXT(stored=True), content=TEXT)
+  index = ix.create_in(index_full_path, schema)
+  writer = index.writer()
+  for note in os.listdir(notes_path):
+    note_full_path = os.path.join(notes_path, note)
+    with open(note_full_path) as note_file:
+      writer.add_document(filename=unicode(note_full_path, "utf-8"), content=unicode(note_file.read(), "utf-8"))
+  return index
 
 def load_config_from(path):
   with open(expanduser(path)) as file:
@@ -17,19 +30,17 @@ def parse_options():
   parser.add_argument("--query")
   return parser.parse_args()
 
-def create_or_load_index(index_path):
+def create_or_load_index(index_path, notes_path):
   index_full_path = expanduser(index_path)
   if isdir(index_full_path):
     return ix.open_dir(index_full_path)
   else:
-    os.mkdir(index_full_path)
-    schema = Schema(filename=TEXT(stored=True), content=TEXT)
-    return ix.create_in(index_full_path, schema)
+    return command_reindex(index_full_path, notes_path)
 
 def main():
   config = load_config_from("~/.notes-cli/config.yaml")
   options = parse_options()
-  index = create_or_load_index(config["indexdir"])
+  index = create_or_load_index(config["indexdir"], expanduser(config["notesdir"]))
   print options
 
 if __name__ == "__main__":
