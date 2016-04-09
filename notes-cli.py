2a7322104eb4222517f8a1167597104c5daab0bc
Paulo Schneider <paulo.schneider@gmail.com>
2016-04-09 23:13:54 +0100

    Create or load index directory

--- notes-cli.py
+++ notes-cli.py
@@ -1,6 +1,9 @@
 import argparse
 import yaml
-from os.path import expanduser
+import os
+from os.path import expanduser, isdir
+import whoosh.index as ix
+from whoosh.fields import *
 
 
 def load_config_from(path):
@@ -14,9 +17,19 @@ def parse_options():
   parser.add_argument("--query")
   return parser.parse_args()
 
+def create_or_load_index(index_path):
+  index_full_path = expanduser(index_path)
+  if isdir(index_full_path):
+    return ix.open_dir(index_full_path)
+  else:
+    os.mkdir(index_full_path)
+    schema = Schema(filename=TEXT(stored=True), content=TEXT)
+    return ix.create_in(index_full_path, schema)
+
 def main():
   config = load_config_from("~/.notes-cli/config.yaml")
   options = parse_options()
+  index = create_or_load_index(config["indexdir"])
   print options
 
 if __name__ == "__main__":
