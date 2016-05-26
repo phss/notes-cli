683ef6b524b22113e35391373231e93d15d23e3e
Paulo Schneider <paulo.schneider@gmail.com>
2016-05-26 13:10:42 +0100

    Test config loading

---
+++ tests/test_config.py
@@ -0,0 +1,9 @@
+import unittest
+from notescli import config
+
+class TestConfig(unittest.TestCase):
+
+  def test_load_config_with_full_path(self):
+    loaded_conf = config.load_config("tests/fixtures/sample-config.yaml")
+    self.assertEquals(loaded_conf["indexdir"], "/tmp/some/index/dir")
+    self.assertEquals(loaded_conf["notesdir"], "/tmp/some/notes/dir")
