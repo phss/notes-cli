c2f1db25a0fe22668464dfa157a92f9541f81af6
Paulo Schneider <paulo.schneider@gmail.com>
2016-05-27 13:55:45 +0100

    Config loading return object instead of dictionary

--- notescli/config.py
+++ notescli/config.py
@@ -1,6 +1,11 @@
 import yaml
 from os.path import expanduser
+from collections import namedtuple
+
+Config = namedtuple("Config", "indexdir notesdir")
 
 def load_config(path):
   with open(expanduser(path)) as file:
-    return yaml.load(file)
+    config_dict = yaml.load(file)
+    return Config(config_dict["indexdir"], config_dict["notesdir"])
+
--- tests/test_config.py
+++ tests/test_config.py
@@ -5,5 +5,5 @@ class TestConfig(unittest.TestCase):
 
   def test_load_config_with_full_path(self):
     loaded_conf = config.load_config("tests/fixtures/sample-config.yaml")
-    self.assertEquals(loaded_conf["indexdir"], "/tmp/some/index/dir")
-    self.assertEquals(loaded_conf["notesdir"], "/tmp/some/notes/dir")
+    self.assertEquals(loaded_conf.indexdir, "/tmp/some/index/dir")
+    self.assertEquals(loaded_conf.notesdir, "/tmp/some/notes/dir")
