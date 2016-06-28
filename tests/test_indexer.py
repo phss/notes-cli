a3b870bec3b7817c313f3f6ccc7a3675bffdc0f6
Paulo Schneider <paulo.schneider@gmail.com>
2016-06-28 09:43:10 +0100

    Test for loading index

--- tests/test_indexer.py
+++ tests/test_indexer.py
@@ -3,7 +3,7 @@ import os
 import tempfile
 from notescli import config as c, indexer
 
-class TestIndexer(unittest.TestCase):
+class TestCreateIndex(unittest.TestCase):
 
   def test_create_index_from_scratch(self):
     index_dir = tempfile.mktemp(prefix='notestest-')
@@ -15,7 +15,14 @@ class TestIndexer(unittest.TestCase):
     # TODO: add better assertion at some point
     
   def test_load_index_if_already_created(self):
-    None
+    index_dir = tempfile.mktemp(prefix='notestest-')
+    config = c.Config(index_dir, "tests/fixtures/docs_to_index/")
+
+    indexer.create_or_load_index(config)
+    index = indexer.create_or_load_index(config)
+
+    self.assertIsNotNone(index) 
+    # TODO: add better assertion at some point
 
   def test_fail_to_create_if_notes_path_dont_exist(self):
     None
