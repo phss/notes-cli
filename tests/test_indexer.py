1b78274674a282fde5b9b97c609fd534d9295fa3
Paulo Schneider <paulo.schneider@gmail.com>
2016-06-28 09:42:26 +0100

    Assert index is created

--- tests/test_indexer.py
+++ tests/test_indexer.py
@@ -8,8 +8,11 @@ class TestIndexer(unittest.TestCase):
   def test_create_index_from_scratch(self):
     index_dir = tempfile.mktemp(prefix='notestest-')
     config = c.Config(index_dir, "tests/fixtures/docs_to_index/")
+
     index = indexer.create_or_load_index(config)
-    # assert something
+
+    self.assertIsNotNone(index) 
+    # TODO: add better assertion at some point
     
   def test_load_index_if_already_created(self):
     None
