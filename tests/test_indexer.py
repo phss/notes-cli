b7cf38978818f00886997d821dd2be874d8daa15
Paulo Schneider <paulo.schneider@gmail.com>
2016-06-28 09:58:22 +0100

    Extract method to create config with temp index dir

--- tests/test_indexer.py
+++ tests/test_indexer.py
@@ -6,24 +6,26 @@ from notescli import config as c, indexer
 class TestCreateIndex(unittest.TestCase):
 
   def test_create_index_from_scratch(self):
-    index_dir = tempfile.mktemp(prefix='notestest-')
-    config = c.Config(index_dir, "tests/fixtures/docs_to_index/")
+    config = self._config_with_temp_index_dir("tests/fixtures/docs_to_index/")
 
     index = indexer.create_or_load_index(config)
 
-    self.assertIsNotNone(index) 
+    self.assertIsNotNone(index)
     # TODO: add better assertion at some point
-    
+
   def test_load_index_if_already_created(self):
-    index_dir = tempfile.mktemp(prefix='notestest-')
-    config = c.Config(index_dir, "tests/fixtures/docs_to_index/")
+    config = self._config_with_temp_index_dir("tests/fixtures/docs_to_index/")
 
     indexer.create_or_load_index(config)
     index = indexer.create_or_load_index(config)
 
-    self.assertIsNotNone(index) 
+    self.assertIsNotNone(index)
     # TODO: add better assertion at some point
 
   def test_fail_to_create_if_notes_path_dont_exist(self):
     None
+
+  def _config_with_temp_index_dir(self, notes_dir):
+    index_dir = tempfile.mktemp(prefix='notestest-')
+    return c.Config(index_dir, notes_dir)
 
