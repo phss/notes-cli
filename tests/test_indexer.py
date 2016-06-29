abb49af942f87e234223287d2af330546cc5476d
Paulo Schneider <paulo.schneider@gmail.com>
2016-06-29 15:41:56 +0100

    Failing test for creating index on non empty dir

--- tests/test_indexer.py
+++ tests/test_indexer.py
@@ -29,12 +29,21 @@ class TestCreateIndex(unittest.TestCase):
 
     self.assertIsNotNone(index)
 
+  def test_fail_to_create_if_index_dir_contains_non_index(self):
+    config = c.Config("tests/fixtures/docs_to_index", "tests/fixtures/docs_to_index")
+
+    index = indexer.create_or_load_index(config)
+
+    self.assertIsNone(index) # should throw exception
+
   def test_fail_to_create_if_notes_path_dont_exist(self):
     config = self._config_with_temp_index_dir("/tmp/no/notes/dir")
 
     index = indexer.create_or_load_index(config)
 
     self.assertIsNone(index)
+
+
 
   def _config_with_temp_index_dir(self, notes_dir):
     index_dir = tempfile.mktemp(prefix='notestest-')
